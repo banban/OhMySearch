063d390cf2add8f6dcf4dd2c3cf4110a1d2e2ffb
Andrew Butenko <andrew.butenko@novasystems.com>
2016-07-19 22:09:04 +0930

    improve Pager component

---
+++ FrontEnd/Search.Core.Windows/ViewComponents/Pager.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Search.Core.Windows.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Search.Core.Windows.ViewComponents
+{
+    public class Pager : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(Models.Query query)
+        {
+            if (query == null)
+            {
+                query = new Models.Query();
+            }
+            //Models.Query query = new Models.Query();
+            if (string.IsNullOrEmpty(query.QueryTerm) && !string.IsNullOrEmpty(Request.Query["term"]))
+            {
+                query.QueryTerm = Request.Query["term"];
+            }
+            if (string.IsNullOrEmpty(query.ChosenOptions) && !string.IsNullOrEmpty(Request.Query["options"]))
+            {
+                query.ChosenOptions = Request.Query["options"];
+            }
+            if (string.IsNullOrEmpty(query.ChosenAggregations) && !string.IsNullOrEmpty(Request.Query["aggregations"]))
+            {
+                query.ChosenAggregations = Request.Query["aggregations"];
+            }
+            if (query.QueryOptions.Count() == 0)
+            {
+                query.QueryOptions = await Controllers.QueryController.GetQueryOptions(query.ChosenOptions);
+            }
+
+            return View(query);
+        }
+    }
+}
