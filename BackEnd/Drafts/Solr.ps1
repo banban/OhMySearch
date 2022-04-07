05f0f0fd04c66a4aedd5c153ba344688031fecde
banban <butenko.andrew@gmail.com>
2022-04-07 14:24:09 +0930

    Update Solr.ps1

--- BackEnd/Drafts/Solr.ps1
+++ BackEnd/Drafts/Solr.ps1
@@ -11,9 +11,9 @@
 #bin/solr create -c library_core
 #get-psdrive -psprovider filesystem
 #java -Dc=library -Drecursive=true -jar "C:\Search\solr-5.4.0\example\exampledocs\post.jar" "L:\"
-#java -Dc=library -Drecursive=yes -Ddata=files -jar "C:\Search\solr-5.4.0\example\exampledocs\post.jar" "L:\*.*" #"\\nova\nova-dfs\library\" 
+#java -Dc=library -Drecursive=yes -Ddata=files -jar "C:\Search\solr-5.4.0\example\exampledocs\post.jar" "L:\*.*" #"\\dfs\library\" 
 #java -Dc=library -Drecursive -Dauto=true -jar C:\Search\solr-5.4.0\example\exampledocs\post.jar L:\_Instructions\ #-filetypes ppt,html
-java -Dc=library_core -Ddata=files -Drecursive -Dauto=true -jar C:\Search\solr-5.4.0\example\exampledocs\post.jar \\nova\nova-dfs\library\_Instructions\
+java -Dc=library_core -Ddata=files -Drecursive -Dauto=true -jar C:\Search\solr-5.4.0\example\exampledocs\post.jar \\dfs\library\_Instructions\
 java -Dc=library_core -Ddata=files -Drecursive -Dauto=true -jar C:\Java\solr-5.3.1\example\exampledocs\post.jar C:\Temp
 
 java -jar C:\Search\solr-5.4.0\server\start.jar --help
